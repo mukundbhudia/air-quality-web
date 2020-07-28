2ca04f9894da184afc638bb3aacedac577bda8e9
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-07-28 15:02:57 +0100

    Added retrieval of multiple cities at once

--- src/components/panels/HomePanel.tsx
+++ src/components/panels/HomePanel.tsx
@@ -1,26 +1,38 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import * as timeago from 'timeago.js'
+
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
 
 import { URI_ENDPOINT } from '../../endpointConnection'
 
+const setParams = (params: any): URLSearchParams => {
+  const searchParams = new URLSearchParams()
+  searchParams.set("stnIds", params.stnIds.toString())
+  return searchParams
+}
+
+let allStations: Array<string> = ['5724', '9974', '5115', '5722', '7024', '1451']
+
 const HomePanel = () => {
   const [error, setError] = useState(null)
-  const [isLoaded, setIsLoaded] = useState(false)
-  const [aqi, setAqi] = useState<string>('')        // TODO: Use some kind of stateful data structure
-  const [city, setCity] = useState<string>('')
-  const [lastUpdated, setLastUpdated] = useState<string>('')
+  const [isLoaded, setIsLoaded] = useState<boolean>(false)
+  const [items, setItems] = useState<Array<any>>([])
 
   useEffect(() => {
-    axios(`${URI_ENDPOINT}/getStation/`, {
-      params: { stnId: 5724 }
+    const params = setParams( { stnIds: allStations } )
+    axios(`${URI_ENDPOINT}/getMultipleStations/`, {
+      params
     })
     .then((result) => {
         setIsLoaded(true)
-        setAqi(result.data.data.aqi)
-        setCity(result.data.data.city.name)
-        setLastUpdated(result.data.data.time.iso)
+        const nonErrorStations = result &&
+          result.data &&
+          result.data.data.filter((item: any) => {
+            return !('err' in item)
+          })
+        setItems(nonErrorStations)
     })
     .catch((error) => {
       setIsLoaded(true)
@@ -32,52 +44,46 @@ const HomePanel = () => {
     return <div>Error accessing data.</div>
   } else if (!isLoaded) {
     return (      
-    <>
-      <Col>
-        <Card className="text-center">
-          <Card.Body>
-            <Card.Title>Loading...</Card.Title>
-            <Card.Text>
-              Loading...
-            </Card.Text>
-            <Card.Text>
-              <small className="text-muted">Loading...</small>
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </Col>
-    </>
+      <>
+        {allStations.map((_, i) => {            
+          return (
+            <Col key={ i }>
+              <Card className="text-center">
+                <Card.Body>
+                  <Card.Title>Loading ...</Card.Title>
+                  <Card.Text>
+                    Air quality index: Loading ...
+                  </Card.Text>
+                  <Card.Text>
+                    <small className="text-muted">Last updated: Loading ...</small>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          )})
+        }
+      </>
     )
   } else {
     return (
       <>
-        <Col>
-          <Card className="text-center">
-            <Card.Body>
-              <Card.Title>{ city }</Card.Title>
-              <Card.Text>
-                Air quality index: { aqi }
-              </Card.Text>
-              <Card.Text>
-                <small className="text-muted">Last updated: { (new Date(lastUpdated)).toLocaleString() }</small>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        {/* <Col>
-          <Card className="text-center">
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This card has supporting text below as a natural lead-in to additional
-                content.{' '}
-              </Card.Text>
-              <Card.Text>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col> */}
+        {items.map(item => {            
+          return (
+            <Col key={ item.idx }>
+              <Card className="text-center">
+                <Card.Body>
+                  <Card.Title>{ item.city.name }</Card.Title>
+                  <Card.Text>
+                    Air quality index: { item.aqi }
+                  </Card.Text>
+                  <Card.Text>
+                    <small className="text-muted">Last updated: { timeago.format(new Date(item.time.iso)) }</small>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          )})
+        }
       </>
     )
   }
